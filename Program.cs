37b57fb5f938ef5962d439c3ddca623ead969f24
Heather Herbert <heather.herbert.1975@gmail.com>
2021-05-16 21:56:50 +0100

    basic code implementation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace Text_to_speech
 {
@@ -6,7 +12,25 @@ namespace Text_to_speech
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fileName = @"settings.txt";
+            
+            string[] lines = File.ReadAllLines(fileName);
+
+            SynthesizeAudioAsync(lines[0], lines[1], lines[2]);
         }
+
+        static async Task SynthesizeAudioAsync(string YourSubscriptionKey, string YourServiceRegion, string textToSay)
+        {
+            var config = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+            config.SetSpeechSynthesisOutputFormat(SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm);
+
+            using var synthesizer = new SpeechSynthesizer(config, null);
+            var result = await synthesizer.SpeakTextAsync(textToSay);
+
+            using var stream = AudioDataStream.FromResult(result);
+            await stream.SaveToWaveFileAsync("./file.wav");
+        }
+
+
     }
 }
