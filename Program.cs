eb420e8e89b24b0ad8aefe20195a6df40b5835c9
Heather Herbert <heather.herbert.1975@gmail.com>
2021-05-16 20:38:47 +0100

    stub

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Text_to_speech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
